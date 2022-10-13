920a133f5eb4c0401ea3eb83f01a1d57668b5d04
Divinelewis <divinitysoft0@yahoo.com>
2022-10-13 14:39:26 -0700

    C Programming code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,13 +10,15 @@ int main(void)
 {
 	char c;	
 	int i;
+	long int l;
+	long long int y;
 	float f;
 	
 	printf("size of char: %zu byte(s)\n", sizeof(c));
 	printf("size of an int: %zu byte(s)\n", sizeof(i));
-	printf("size of a long int: %zu byte(s)\n", sizeof(i));
-	printf("size of a long long int: %zu byte(s)\n", sizeof(i));
-	printf("size of a float: %zu byte(s)\n", size of (f));
+	printf("size of a long int: %zu byte(s)\n", sizeof(l));
+	printf("size of a long long int: %zu byte(s)\n", sizeof(y));
+	printf("size of a float: %zu byte(s)\n", sizeof(f));
 	
 	return (0);
 }
