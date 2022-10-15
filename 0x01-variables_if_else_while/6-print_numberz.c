d598989cb9e304acb155ee0b0cd5b4d00682044c
Divinelewis <divinitysoft0@yahoo.com>
2022-10-14 20:21:30 -0700

    My commit messsge

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - prints numbers of base 10 from 0
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i + '0');
+		++i;
+	}
+	putchar('\n');
+	return (0);
+}
