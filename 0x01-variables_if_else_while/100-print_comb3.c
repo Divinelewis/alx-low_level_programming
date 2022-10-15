b936f53aa49e3a5c27e2fabb2bafccdd138f3774
Divinelewis <divinitysoft0@yahoo.com>
2022-10-15 03:35:52 -0700

    My C Commit for C Programming

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - prints numbers of base 10 from 0
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n <= 56; n++)
+	{
+		if (n < m)
+		{
+			putchar(n);
+			putchar(m);
+			if (n != 56 || m != 57)
+			{
+				putchar(',');
+				putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
