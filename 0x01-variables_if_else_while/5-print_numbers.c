f993e3ec01179401a358fb2ad72c80d8b9d65676
Divinelewis <divinitysoft0@yahoo.com>
2022-10-14 19:19:58 -0700

    My C Commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - prints numbers of base 10 from 0
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	print('\n');
+	return (0);
+}
