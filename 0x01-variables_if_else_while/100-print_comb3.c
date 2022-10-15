08dfafb5e35dc8364513efa9bddc48c29a0bb0b4
Divinelewis <divinitysoft0@yahoo.com>
2022-10-15 03:42:01 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,14 +11,19 @@ int main(void)
 
 	for (n = 48; n <= 56; n++)
 	{
-		if (n < m)
+		for (m = 49; m <= 57; m++)
 		{
-			putchar(n);
-			putchar(m);
-			if (n != 56 || m != 57)
+			if (n < m)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
