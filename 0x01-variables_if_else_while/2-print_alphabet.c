1db08446ec37e37f9a8e067d47e5bbd884309436
Divinelewis <divinitysoft0@yahoo.com>
2022-10-14 18:05:14 -0700

    My C programme

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - Entry point
  * putchar - to write string to the alphabet
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	char ch = 'a';
+
 	while (ch <= 'z')
 	{
 		putchar(ch);
