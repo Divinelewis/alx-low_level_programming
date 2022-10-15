c3a3549b5d8940051e939117c6f834db770d7513
Divinelewis <divinitysoft0@yahoo.com>
2022-10-14 18:53:53 -0700

    A for loop at work

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,14 +7,13 @@
 
 int main(void)
 {
-	char ch = 'a';
+	char ch;
 
-	while (ch <= 'z')
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		if (ch == 'e' && ch == 'q')
-			++ch;
+		continue;
 		putchar(ch);
-		++ch;
 	}
 	putchar('\n');
 	return (0);
