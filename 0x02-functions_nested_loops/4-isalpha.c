08cd6dae5d76b9a5ee290a0487c06e87a479d27e
Divinelewis <divinitysoft0@yahoo.com>
2022-10-18 16:09:23 -0700

    my gut commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - chevks for alphabetic characteds
+ * Return: 0 if successful
+ */
+
+int _isalpha(int c);
+{
+	if (c >= 'a' && c < 'z');
+	{
+		return (1);
+	} else if ( c >= 'A' && c <= 'Z')
+	{
+		return (1))
+	}
+	return (0);
+}
+ 
