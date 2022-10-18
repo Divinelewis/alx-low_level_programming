563a87cefd676ad4adcf07f950b15d21e0013121
Divinelewis <divinitysoft0@yahoo.com>
2022-10-18 10:34:27 -0700

    my git commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: always 0 success
+ */
+
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n')
+
+	return (0);
+}
