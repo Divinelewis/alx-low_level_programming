546cebe03caac070db93120f490c727bad3dc251
Divinelewis <divinitysoft0@yahoo.com>
2022-10-19 01:50:42 -0700

    my git commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,20 +11,18 @@ void times_table(void)
 
 	for (digit = 0; digit <= 9; digit++)
 	{
-		putchar('digit');
+		putchar('0');
 		for (mult = 1; mult <= 9; mult++)
 		{
+			_putchar(',')
 			_putchar(' ');
 			result = digit * mult;
 			if (result <= 9)
-			{
 				_putchar(' ');
-			} else
-			{
+			else
 				_putchar((result / 10) + '0');
 			_putchar((result % 10) + '0');
-			}
-		_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
