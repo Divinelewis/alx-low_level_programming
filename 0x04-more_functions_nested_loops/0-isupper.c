49bc206f5cd76ffb43f4e0d68bfd016128d8f7fe
Divinelewis <divinitysoft0@yahoo.com>
2022-10-20 07:28:37 -0700

    my git commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _isupper - checks for uppercase characters
@@ -8,11 +9,7 @@
 
 int _isupper(int c);
 {
-	if (c >='a' && c < 'z')
-	{
-		return (1);
-	}
-	else if (c >= 'A' && c <= 'Z')
+	if (c >='A' && c < 'Z')
 	{
 		return (1);
 	}
