4a7d27e2daf4be759bfc90dc583b30946cf7a98e
Divinelewis <divinitysoft0@yahoo.com>
2022-10-21 05:41:40 -0700

    my git commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two int
+ * @int a: first int
+ * @int a: second int
+ * Return: success
+ */
+
+int mul(int a, int b)
+{
+	_putchar("%d", a * b);
+	return (0);
+}
