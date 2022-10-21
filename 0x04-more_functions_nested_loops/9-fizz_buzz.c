1b9dd7679d522826b291d195bfda747b52625308
Divinelewis <divinitysoft0@yahoo.com>
2022-10-21 12:08:52 -0700

    my git commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,25 +8,34 @@
 
 int main(void)
 {
-	int num;
+	int i = 1;
 
-	while (num++ < 100)
-		if ((num % 3 == 0) && (num % 5 == 0))
-			printf("FizzBuzz ");
-
-		else if ((num % 3) == 0)
-			printf("Fizz ");
-
-		else if ((num % 5) == 0)
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			if (num != 100)
-				printf("Buzz ");
-			else
-				printf("Buzz");
+			printf("FizzBuzz");
 		}
-	printf("%d ", num);
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%i", i);
+		}
+		if (i != 100)
+		{
+			putchar(' ');
+		}
 
-	printf("\n");
+		i++;
+	}
+	putchar('\n');
 	return (0);
 }
 
