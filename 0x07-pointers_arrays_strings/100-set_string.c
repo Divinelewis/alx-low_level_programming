b831672cc767992412355bca56c9351ffba87a9b
Divinelewis <divinitysoft0@yahoo.com>
2022-10-31 08:29:21 -0700

    my git commit

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * main - Prints 3 combination of numbers
+ *
+ * Return: Always (Success)
+ */
+
+void set_string(char **s, char *to)
+{
+	int c, i, k;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if (c < i && i < k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(k);
+
+					if (c != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
