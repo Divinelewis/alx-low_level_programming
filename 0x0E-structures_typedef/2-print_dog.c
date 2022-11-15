97049483777c14ea0e45bd2c9e1c1b36c7f980bf
Divinelewis <divinitysoft0@yahoo.com>
2022-11-15 00:52:42 -0800

    my git commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 
 /**
@@ -10,7 +12,7 @@
 
 void print_dog(struct dog *d)
 {
-	if (!d || d == NULL)
+	if (d == NULL)
 		return;
 
 	if (d->name == NULL)
@@ -21,10 +23,13 @@ void print_dog(struct dog *d)
 
 	if (d->age < 0)
 		printf("Age: (nil)\n");
+
 	else
 		printf("Age: %f\n", d->age);
+
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
+
 	else
 		printf("Owner: %s\n", d->owner);
 }
